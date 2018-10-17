cfb9174de9d6cd7a27d36099aeda806ea190cf3a
fellyph <fellyph.cintra@gmail.com>
2018-10-17 07:40:25 +0100

    adding conditional for custom field

--- templates/singular.php
+++ templates/singular.php
@@ -3,12 +3,14 @@
   <article class="postCard">
     <div class="postCard__info">
       <?php the_content(); ?>
-      <p class="postCard__level">
-        Nivel do post: 
-        <span class="postCard__level_item">
-          <?php echo get_post_meta( get_the_ID(), 'post_level', true); ?>
-        </span>
-      </p>
+      <?php if(get_post_meta( get_the_ID(), 'post_level', true)): ?>
+        <p class="postCard__level">
+          Nivel do post: 
+          <span class="postCard__level_item">
+            <?php echo get_post_meta( get_the_ID(), 'post_level', true); ?>
+          </span>
+        </p>
+      <?php endif; ?>
     </div>
     <footer class="postCard__footer">
       <a href="<?php bloginfo('url'); ?>">Voltar para a home</a>
