1a703418c384b20649a60f0029ab4ff6ecc5b2ca
fellyph <fellyph.cintra@gmail.com>
2018-10-17 07:56:24 +0100

    adding conditional to show only on specific category

--- templates/singular.php
+++ templates/singular.php
@@ -2,8 +2,17 @@
   <h2 class="posts__title singlePage__title"><?php the_title(); ?></h2>
   <article class="postCard">
     <div class="postCard__info">
+      Categorias: <?php the_category(',') ?>
       <?php the_content(); ?>
-      <?php if(get_post_meta( get_the_ID(), 'post_level', true)): ?>
+      <?php 
+          // is_category()
+          // para queries de paginas de arquivo
+          
+          // has_category()
+          // pagina single
+      ?>
+      
+      <?php if(get_post_meta( get_the_ID(), 'post_level', true) && has_category('tutorial')): ?>
         <p class="postCard__level">
           Nivel do post: 
           <span class="postCard__level_item">
@@ -11,6 +20,7 @@
           </span>
         </p>
       <?php endif; ?>
+
     </div>
     <footer class="postCard__footer">
       <a href="<?php bloginfo('url'); ?>">Voltar para a home</a>
