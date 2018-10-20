cae445320e921ad8fa30f883b2b06ac703669e48
fellyph <fellyph.cintra@gmail.com>
2018-10-20 09:58:02 +0100

    adding info from the object post

--- templates/card-page.php
+++ templates/card-page.php
@@ -4,6 +4,16 @@
     <div class="postCard__info">
       <?php the_content(); ?>
     </div>
+
+    <div class="postCard__info">
+      <?php 
+        $extras = get_field('extras');
+        //print_r($extras);
+
+        echo '<h3>' . $extras->post_title . '</h3>';
+        echo '<div>' . $extras->post_content .  '</div>';
+      ?>
+    </div>
     <footer class="postCard__footer">
       <a href="<?php bloginfo('url'); ?>">Voltar para a home</a>
     </footer>
