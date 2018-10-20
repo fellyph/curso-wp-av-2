5b4e85c797ff7fb845e1f20632b79d85d3fb0c4e
fellyph <fellyph.cintra@gmail.com>
2018-10-20 10:44:39 +0100

    adding multiple items

--- templates/card-page.php
+++ templates/card-page.php
@@ -6,24 +6,29 @@
     </div>
 
     <?php
-      $post = get_field('extras');
-      if($post):
+      $extras = get_field('extras');
+      if($extras):
     ?>
-    <details class="postExtra">
-      <?php 
-        
-        // setup_postdata
-        // define que o dado que a gente vai trabalhar 
-        // é um post
-        setup_postdata($post); ?>
+      <div class="postCard__info">
+        <h2>Saiba mais</h2>
+        <?php foreach($extras as $post): ?>
+          <details class="postExtra">
+            <?php 
+              
+              // setup_postdata
+              // define que o dado que a gente vai trabalhar 
+              // é um post
+              setup_postdata($post); ?>
+              <summary class="postExtra__title"> <?php the_title() ?> </summary>
+              <div class="postExtra__content">
+                <?php the_content(); ?>
+              </div>
+            <?php wp_reset_postdata();
+            ?>
+          </details>
+        <?php endforeach; ?>
 
-        <summary class="postExtra__title"> <?php the_title() ?> </summary>
-        <div class="postExtra__content">
-          <?php the_content(); ?>
-        </div>
-        <?php wp_reset_postdata()
-      ?>
-    </details>
+      </div>
     <?php endif; ?>
     <footer class="postCard__footer">
       <a href="<?php bloginfo('url'); ?>">Voltar para a home</a>
