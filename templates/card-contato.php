198ddc4514e75fc4dac7bf52c1bfe03f8fd9066b
fellyph <fellyph.cintra@gmail.com>
2018-10-20 11:17:22 +0100

    reading address data

--- templates/card-contato.php
+++ templates/card-contato.php
@@ -1,9 +1,15 @@
-<article <?php post_class("postCard"); ?>>
-  <?php the_post_thumbnail('thumb-post'); ?>
-  <div class="postCard__info">
-    <h2 class="postCard__title"><?php the_title(); ?></h2>
-    <div class="postCard__content">
-      <?php the_content(); ?>
+<div class="contactPage">
+  <article <?php post_class("postCard"); ?>>
+    <?php the_post_thumbnail('thumb-post'); ?>
+    <div class="postCard__info">
+      <h2 class="postCard__title"><?php the_title(); ?></h2>
+      <div class="postCard__content">
+        <?php the_content(); ?>
+      </div>
     </div>
-  </div>
-</article>
+  </article>
+
+  <aside class="addressCard">
+    <?php the_field('endereco'); ?>
+  </aside>
+</div>
